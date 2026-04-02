3f11c1f76a2dba9b695105acd1aa27b936e55665
Jaydin Morrison <4260354@myuwc.ac.za>
2026-04-02 10:37:28 +0200

    fix: update API base URL in portal-config and enhance Content Security Policy

--- assets/portal-config.js
+++ assets/portal-config.js
@@ -1,2 +1,2 @@
-window.__PO_API_BASE__ = "https://fuzzy-cod-vjj99v4r44vfx5xp-3001.app.github.dev";
+window.__PO_API_BASE__ = "https://api.projectodysseus.live";
 window.__ODIE_ACCESS_KEY__ = "6e99edb286ac0b623e02fa1bfd8e5422114d1d80e690582071ce6b2fa9d2b43b";
